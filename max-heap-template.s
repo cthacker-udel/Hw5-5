7e866e94312cc45926d407e94c755f4a3287f844
bgallamoza <brennan.gallamoza@gmail.com>
2021-11-04 11:59:27 -0400

    implemented insert

--- max-heap-template.s
+++ max-heap-template.s
@@ -16,7 +16,7 @@ main:
 	ldr r0, [r1]
 	swi 0x6c	@ read an integer put in r0
 
-	@ To-Do: Here you should creat a base node (ROOT NODE) containing the just-read integer for your MaxHeap
+	@ To-Do 1: Here you should creat a base node (ROOT NODE) containing the just-read integer for your MaxHeap
 	@ and save the base node address to the label MyHeap (which is declared in .data) for later references	
 	mov R2, R0			@ store int read into r2
 	mov R0, #12			@ allocate 12 bytes
@@ -36,7 +36,7 @@ main:
 	swi 0x6c	@ read an integer put in r0
 	BCS CloseF	@ branch when the end of the file is reached
 
-	@ TO-DO: You should comment out the above code for printing
+	@ TO-DO 2: You should comment out the above code for printing
 	@ Instead, you creat a new node and save the integer into the first 4 bytes of the node
 	@ Put the base node address in r0, and the address of the to-be-inserted node in r1
 	@ call the subroutine Insert to insert the newly created node into the MaxHeap
@@ -70,16 +70,39 @@ exit:	SWI 0x11		@ Stop program execution
 
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-@ TO-DO: write the Insert function below
+@ TO-DO 3: write the Insert function below
 @ The function takes two arguments: a pointer to the heap (in r0) and a pointer to a new node to be inserted to the heap (in r1) 
 @ The function returns (in r0) a pointer to the root node (potentially can be the new node) of the heap
-Insert: 
+Insert:
+	ldr r2, [r0, #0]	@ get integer of base
+	ldr r3, [r1, #0]	@ get integer of insert
+	ldr r4, [r0, #4]	@ get left child of parent
+	ldr r5, [r0, #8]	@ get right child of parent
+	cmp r2, r3			@ compare node integers
+	ble lessThan
+	b greaterThan
 
+@ called when insert's integer is less than the root
+lessThan:
+	cmp r4, #0			@ check if base left child is null
+	streq r1, [r0, #4]
+	moveq pc, r14
+	cmp r5, #0			@ check if base right child is null
+	streq r1, [r0, #8]
+	moveq pc, r14
+	str r1, [r0, #4]	@ if both children not NULL, replace left child with insert
+	str r4, [r1, #8]	@ the old left child is now the insert's right child
+	mov pc, r14
+	
+@ called when insert's integer is larger than the root
+greaterThan:
+	str r1, [=MyHeap, #0]
+	str r0, [r1, #4]
 	mov pc, r14
 	
 @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
-@ TO_DO: write deleteMax function below
+@ TO_DO 4: write deleteMax function below
 @ call-by-reference: the function takes a pointer-to-pointer as argument (in r0)
 @ when the heap contains only one node (i.e., the root node), 
 @ deleteMax should return root.data (to r0) and nullify the pointer to the root node
